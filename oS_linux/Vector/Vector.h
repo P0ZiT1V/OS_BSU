9b54461315cf3e13c26f9bfa03ddf440e90ab47b
Ваше Имя <you@example.com>
2022-03-10 02:03:11 +0300

    ini

---
+++ oS_linux/Vector/Vector.h
@@ -0,0 +1,20 @@
+#ifndef VECTOR_VECTOR_H
+#define VECTOR_VECTOR_H
+
+
+#include "Number.h"
+#include <iostream>
+class  Vector {
+public:
+	Number X;
+	Number Y;
+	Vector(Number X, Number Y);
+	Vector sum(Vector one, Vector two);
+	const Vector operator+(const Vector left);
+	Number r();
+	Number angle();
+	void print_vector();
+};
+const Vector ZERO_Vect(Number(0),Number(0));
+const Vector ONE_Vect(Number(1), Number(1));
+#endif
